07aca2901ec2e7e567e01e191a7afd140ccd8c84
Neeraj Bopalkar <neeraj.bopalkar@gmail.com>
2019-09-02 00:33:54 +0530

    added code

---
+++ Asp.Net_Core_Scott_Allen/OdeToFood.Core/Restaurant.cs
@@ -0,0 +1,11 @@
+﻿
+namespace OdeToFood.Core
+{
+    public class Restaurant
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public CuisineType Cuisine { get; set; }
+    }
+}
---
+++ Asp.Net_Core_Scott_Allen/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,51 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant
+                {
+                    ID = 1,
+                    Name = "Neer's Pizza",
+                    Location ="Vallabhnagar",
+                    Cuisine = CuisineType.Italin
+                },
+                   new Restaurant
+                {
+                    ID = 2,
+                    Name = "Neer's Buritos",
+                    Location ="Barshi",
+                    Cuisine = CuisineType.Mexican
+                },
+                      new Restaurant
+                {
+                    ID = 3,
+                    Name = "Neer's Pav Bhaji",
+                    Location ="Sangli",
+                    Cuisine = CuisineType.Indian
+                }
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+           
+            return restaurants.OrderBy(item => item.Name);
+        }
+    }
+}
