75a6b7face536a1424bffca547b04a6e4020b321
Neeraj Bopalkar <neeraj.bopalkar@gmail.com>
2019-11-10 13:17:29 +0530

    added code for adding/getting products from local disk in product catalog service

---
+++ AzureServiceFabric/ECommerce/ECommerce.ProductCatalog.Model/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ECommerce.ProductCatalog.Model
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    
+    }
+}
